6d6301fd443528f9f9641f2c2f04681f5c755b5e
Shota Furuya <s1420693@u.tsukuba.ac.jp>
2014-06-30 17:02:21 +0900

    一応完成

--- InvincibleMini4WD/gen/com/example/invinciblemini4wd/R.java
+++ InvincibleMini4WD/gen/com/example/invinciblemini4wd/R.java
@@ -24,7 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080008;
+        public static final int auto_label=0x7f080001;
+        public static final int auto_start=0x7f080002;
+        public static final int auto_stop=0x7f080000;
+        public static final int manual_backward=0x7f080006;
+        public static final int manual_brake=0x7f080007;
+        public static final int manual_forward=0x7f080005;
+        public static final int manual_label=0x7f080004;
+        public static final int manual_stop=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- InvincibleMini4WD/src/com/example/invinciblemini4wd/MainActivity.java
+++ InvincibleMini4WD/src/com/example/invinciblemini4wd/MainActivity.java
@@ -1,15 +1,48 @@
 package com.example.invinciblemini4wd;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+	
+	private TextView auto_label;
+	private TextView manual_label;
+	private Button auto_stop;
+	private Button auto_start;
+	private Button manual_stop;
+	private Button manual_forward;
+	private Button manual_backward;
+	private Button manual_brake;
+	
+	private boolean manual = false;
+	private int stat = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		auto_label = (TextView)findViewById(R.id.auto_label);
+		manual_label = (TextView)findViewById(R.id.manual_label);
+		auto_stop = (Button)findViewById(R.id.auto_stop);
+		auto_start = (Button)findViewById(R.id.auto_start);
+		manual_stop = (Button)findViewById(R.id.manual_stop);
+		manual_forward = (Button)findViewById(R.id.manual_forward);
+		manual_backward = (Button)findViewById(R.id.manual_backward);
+		manual_brake = (Button)findViewById(R.id.manual_brake);
+		
+		auto_stop.setOnClickListener(this);
+		auto_start.setOnClickListener(this);
+		manual_stop.setOnClickListener(this);
+		manual_forward.setOnClickListener(this);
+		manual_backward.setOnClickListener(this);
+		manual_brake.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +50,52 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View view) {
+		switch(view.getId()) {
+			case R.id.auto_stop:
+				manual = false;
+				auto_label.setTextColor(Color.BLACK);
+				manual_label.setTextColor(Color.BLACK);
+				stat = 0;
+				break;
+			case R.id.auto_start:
+				manual = false;
+				auto_label.setTextColor(Color.RED);
+				manual_label.setTextColor(Color.BLACK);
+				stat = 1;
+				break;
+			case R.id.manual_stop:
+				manual = true;
+				auto_label.setTextColor(Color.BLACK);
+				manual_label.setTextColor(Color.BLACK);
+				stat = 2;
+				break;
+			case R.id.manual_forward:
+				manual = true;
+				auto_label.setTextColor(Color.BLACK);
+				manual_label.setTextColor(Color.RED);
+				stat = 3;
+				break;
+			case R.id.manual_backward:
+				manual = true;
+				auto_label.setTextColor(Color.BLACK);
+				manual_label.setTextColor(Color.RED);
+				stat = 4;
+				break;
+			case R.id.manual_brake:
+				manual = true;
+				auto_label.setTextColor(Color.BLACK);
+				manual_label.setTextColor(Color.BLACK);
+				stat = 5;
+				break;
+		}
+		
+		UltraSonicHttpGetTask task = new UltraSonicHttpGetTask(this);
+		task.execute(stat);
+		
 	}
 
 }
